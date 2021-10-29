ba76e1cb6d9f564676a0b3a2f6d43ea54136732c
shoorik007 <31213414+shoorik007@users.noreply.github.com>
2021-10-29 22:00:06 +0300

    kol

--- sites/klo/js/main.js
+++ sites/klo/js/main.js
@@ -1,16 +1,17 @@
 $(function() {
-  $('.main__cases').slick({
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    dots: true,
-    centerMode: true,
-    centerPadding: '250px',
-    variableWidth: true
-      //autoplay: true
+ $('.main__cases').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  dots: true,
+  centerMode: true,
+  centerPadding: '250px',
+  variableWidth: true,
+  infinite: false
+   //autoplay: true
 
 
 
-  })
+ })
 
 
 
